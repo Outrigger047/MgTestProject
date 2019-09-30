c499d513de8f24fe6c53a0fc94d1c12b9d801d54
Andrew Wyshak <andrew.wyshak@gmail.com>
2019-09-29 23:33:11 -0400

    Added missing files

---
+++ MgTestProject/WorldShape.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MgTestProject
+{
+    public class WorldShape
+    {
+        public WorldShape(WorldShapeGeometry geometry, GraphicsDevice graphicsDevice)
+        {
+            Geometry = geometry;
+
+            // Create texture
+            Texture = new Texture2D(graphicsDevice, Geometry.Width, Geometry.Height);
+
+            Color[] colorData = new Color[Geometry.Width * Geometry.Height];
+            for (int i = 0; i < colorData.Length; i++)
+            {
+                colorData[i] = Color.DarkSlateGray;
+            }
+
+            Texture.SetData(colorData);
+        }
+
+        public Texture2D Texture { get; private set; }
+        public WorldShapeGeometry Geometry { get; private set; }
+    }
+}
---
+++ MgTestProject/WorldShapeGeometry.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MgTestProject
+{
+    public class WorldShapeGeometry
+    {
+        private Vector2 position;
+
+        private int width;
+        private int height;
+        private Shapes shapeType;
+
+        private float rotAngle;
+
+        private Color drawColor;
+
+        private bool passable;
+        private bool shouldRender;
+
+        public WorldShapeGeometry(
+            Vector2 position, 
+            int width, 
+            int height, 
+            Shapes shapeType, 
+            float rotAngle, 
+            Color drawColor, 
+            bool passable = false, 
+            bool shouldRender = true)
+        {
+            Position = position;
+            Width = width;
+            Height = height;
+            ShapeType = shapeType;
+            RotAngle = rotAngle;
+            DrawColor = drawColor;
+            Passable = passable;
+            ShouldRender = shouldRender;
+        }
+
+        public Vector2 Position { get => position; private set => position = value; }
+        public int Width { get => width; private set => width = value; }
+        public int Height { get => height; private set => height = value; }
+        public Shapes ShapeType { get => shapeType; private set => shapeType = value; }
+        public float RotAngle { get => rotAngle; private set => rotAngle = value; }
+        public Color DrawColor { get => drawColor; private set => drawColor = value; }
+        public bool Passable { get => passable; private set => passable = value; }
+        public bool ShouldRender { get => shouldRender; private set => shouldRender = value; }
+    }
+
+    public enum Shapes
+    {
+        Rectangle,
+        Ellipse
+    }
+}
